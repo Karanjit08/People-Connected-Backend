3c62f42d855180300175212b6ef2de4a8addc47f
Karanjit08 <karanjitrandhawa2000@gmail.com>
2025-05-13 22:43:56 +0530

    Initial Commit

---
+++ PeopleConnected/src/main/java/com/peopleconnected/PeopleConnected/PeopleConnectedApplication.java
@@ -0,0 +1,14 @@
+package com.peopleconnected.PeopleConnected;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleConnectedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleConnectedApplication.class, args);
+		System.out.println("Running...");
+	}
+
+}
---
+++ PeopleConnected/src/test/java/com/peopleconnected/PeopleConnected/PeopleConnectedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peopleconnected.PeopleConnected;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleConnectedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
