44e3c42d87af58f30926f2af11f721d90316ae8d
Karanjit08 <karanjitrandhawa2000@gmail.com>
2025-05-17 11:12:51 +0530

    test

--- PeopleConnected/src/main/java/com/peopleconnected/PeopleConnected/PeopleConnectedApplication.java
+++ PeopleConnected/src/main/java/com/peopleconnected/PeopleConnected/PeopleConnectedApplication.java
@@ -8,7 +8,7 @@ public class PeopleConnectedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PeopleConnectedApplication.class, args);
-		System.out.println("Running...");
+		System.out.println("Server Running...");
 	}
 
 }
