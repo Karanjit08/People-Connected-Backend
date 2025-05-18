44cbdc559bdcb47ce9c69dec725540ddb01c8eb0
Karanjit08 <karanjitrandhawa2000@gmail.com>
2025-05-18 11:07:13 +0530

    GET (Get all Registered Users) GET Individual Users (Get Individual Users by Name)

--- PeopleConnected/src/main/java/com/peopleconnected/PeopleConnected/dao/UserDao.java
+++ PeopleConnected/src/main/java/com/peopleconnected/PeopleConnected/dao/UserDao.java
@@ -3,6 +3,8 @@ package com.peopleconnected.PeopleConnected.dao;
 import com.peopleconnected.PeopleConnected.entities.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserDao extends JpaRepository<UserProfile,Integer> {
+import java.util.List;
 
+public interface UserDao extends JpaRepository<UserProfile,Integer> {
+    List<UserProfile> findByNameContainingIgnoreCase(String name);
 }
